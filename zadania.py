cc4de78eac33295a027a40d87ce751916f2af878
unknown <you@example.com>
2023-12-04 14:10:34 +0100

    Dodano zadania

--- zadania.py
+++ zadania.py
@@ -9,33 +9,42 @@ Original file is located at
 Zadanie 1
 """
 
+
 def wyswietl_imiona(imiona):
-    if len(imiona) == 5:
+    if len(imiona) != 5:
         for imie in imiona:
             print(imie)
     else:
-        print("Podano niepoprawną liczbę imion. Proszę podać 5 imion.")
+
+        for imie in imiona:
+            print(imie)
+
 
 lista_imion = ["Jakub", "Ola", "Darek", "Łukasz", "Ewa"]
+
 wyswietl_imiona(lista_imion)
 
 """Zadanie 2"""
 
+
 # Wersja 1
-def pomnoz_elementy(lista):
-    wynik = []
-    for liczba in lista:
-        wynik.append(liczba * 2)
-    return wynik
+def pomnoz_elementy(lista1):
+    wynik1 = []
+    for liczba in lista1:
+        wynik1.append(liczba * 2)
+    return wynik1
 
 
-lista = [1, 2, 3, 4, 5]
+lista: List[int] = [1, 2, 3, 4, 5]
 wynik = pomnoz_elementy(lista)
+
 print(wynik)
 
+
 # Wersja 2
-def pomnoz_elementy(lista):
-    return [liczba * 2 for liczba in lista]
+def pomnoz_elementy(lista1):
+    return [liczba * 2 for liczba in lista1]
+
 
 lista = [1, 2, 3, 4, 5]
 wynik = pomnoz_elementy(lista)
@@ -43,19 +52,23 @@ print(wynik)
 
 """Zadanie 3"""
 
+
 def wyswietl_parzyste_elementy(lista):
     for liczba in lista:
         if liczba % 2 == 0:
             print(liczba)
+
 
 lista = list(range(1, 11))
 wyswietl_parzyste_elementy(lista)
 
 """Zadanie 4"""
 
-def wyswietl_co_drugi_element(lista):
-    for i in range(0, len(lista), 2):
-        print(lista[i])
+
+def wyswietl_co_drugi_element(lista1):
+    for i in range(0, len(lista1), 2):
+        print(lista1[i])
+
 
 lista = list(range(1, 11))
 wyswietl_co_drugi_element(lista)
